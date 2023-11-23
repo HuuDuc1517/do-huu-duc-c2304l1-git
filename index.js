9e46651224b634a66734e1d6480d7f92fb53ca1e
do-huu-duc-c2304l1-git <huuduc200999@gmail.com>
2023-11-23 20:26:59 +0700

    tong cac phan tu trong mang

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ let arr = [5, 1, 4, 2, 7];
 for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
 }
+
+//bai4
+
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+
+console.log(sum)
